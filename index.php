574e954c754eb233c295fdd9e0c85b9ed7def37e
William Tang <wtang12340@gmail.com>
2017-02-09 22:53:03 -0500

    More changes, css file added.

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
 	<title>William Tang</title>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="css/william.css"
 </head>
 <body>
 	<div class="container">
@@ -13,10 +14,14 @@
 		<img src='images/william.jpg' alt='William Tang'>
 		<h2>About Me</h2>
 		<p>
+        My name is William Tang and I work as a web developer. I grew up in Boston and has been interested in computer science
+        since high school. I graduated from Boston University 2 years ago with a Computer Science degree and has been getting experience in the field.
+        I want to further improve my skills in web development by filling the holes in my knowledge
+        by earning a Masters degree at Harvard Extension. Nice to meet everyone. 
 		</p>
 		<h2>Random Quote</h2>
 		<blockquote>
-			Do your own thing on your own terms and get what you came here for. -Oliver James		</blockquote>
+		</blockquote>
 	</div>
 </body>
 </html>
