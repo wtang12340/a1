592402905005c9de3febc211f733f0e732f98a7a
William Tang <wtang12340@gmail.com>
2017-02-09 21:43:52 -0500

    Initial changes.

---
+++ index.php
@@ -0,0 +1,22 @@
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>William Tang</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+	<div class="container">
+		<h1>William Tang</h1>
+		<img src='images/william.jpg' alt='William Tang'>
+		<h2>About Me</h2>
+		<p>
+		</p>
+		<h2>Random Quote</h2>
+		<blockquote>
+			Do your own thing on your own terms and get what you came here for. -Oliver James		</blockquote>
+	</div>
+</body>
+</html>
